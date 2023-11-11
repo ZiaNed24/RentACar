08e25e7d84b295ede0f6374094e0a55260f36df3
ZiaNed24 <zianeduet123@gmail.com>
2023-11-12 02:19:13 +0500

    first commit

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 from flask import Flask
 from main import main_blueprint, create_app
 from admin import admin_blueprint
-import secrets
+from decouple import config  # Import config from python-decouple
 
-from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-secret_key = secrets.token_hex(24)  # You can adjust the key length as needed
+
+# Read the secret key from the .env file
+secret_key = config('SECRET_KEY')
 
 # Set the secret key for your Flask application
 app.secret_key = secret_key
-# Configuration for SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/rentacar'
+
+# Read the database URI from the .env file
+app.config['SQLALCHEMY_DATABASE_URI'] = config('SQLALCHEMY_DATABASE_URI')
 
 # Register the main blueprint
-
 app.register_blueprint(main_blueprint)
 app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
 # Initialize the db instance with your Flask app
 create_app(app)
 
